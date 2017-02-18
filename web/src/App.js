800c5b1735d44750d79f415466f8d78e6adc5b90
Bradley Spaulding <brad.spaulding@gmail.com>
2017-02-18 10:56:47 -0800

    Removed CSS for StyleSheet

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
+import { StyleSheet, Text, View } from 'react-native';
+
+const H2 = Text;
+const Code = Text;
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		flex: 1
+	},
+	header: {
+		backgroundColor: '#222',
+		flex: 1,
+		height: '150px',
+		padding: '20px',
+	},
+	headerText: {
+		color: 'white',
+	},
+	intro: {
+		fontSize: 'large'
+	}
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <H2 style={styles.headerText}>Welcome to React</H2>
+        </View>
+        <Text style={styles.intro}>
+          To get started, edit <Code>src/App.js</Code> and save to reload.
+        </Text>
+      </View>
     );
   }
 }
