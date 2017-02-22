3886a123bb722ac0ee0e88425038b10356c6cef0
Bradley Spaulding <brad.spaulding@gmail.com>
2017-02-22 14:51:39 -0800

    Extracted LazyLink, updated SpeciesDetail

--- Demo/src/components/LazyLinkList.js
+++ Demo/src/components/LazyLinkList.js
@@ -5,17 +5,21 @@ import Text from './Text';
 import Request from './Request';
 import { getId } from './ListView';
 
+export const LazyLink = ({ association, url }) => (
+	<Request key={url} url={url}>
+		{(object) => (
+			<Link key={url} to={`/${association}/${getId(association)(url)}`}>
+				<Text>{object.name || object.title}</Text>
+			</Link>
+		)}
+	</Request>
+);
+
 const LazyLinkList = ({ accessor, association, source }) => {
 	return (
 		<View>
 			{source[accessor || association].map(url => (
-				<Request key={url} url={url}>
-					{(object) => (
-						<Link key={url} to={`/${association}/${getId(association)(url)}`}>
-							<Text>{object.name || object.title}</Text>
-						</Link>
-					)}
-				</Request>
+				<LazyLink url={url} association={association}/>
 			))}
 		</View>
 	);
--- Demo/src/components/SpeciesDetail.js
+++ Demo/src/components/SpeciesDetail.js
@@ -3,6 +3,10 @@ import { View } from 'react-native';
 import { getSpecies } from '../api/SWApi';
 import Loading from './Loading';
 import Text from './Text';
+import Heading from './Heading';
+import SectionHeading from './SectionHeading';
+import LazyLinkList, { LazyLink } from './LazyLinkList';
+import { Stats, Stat } from './Stat';
 
 class SpeciesDetail extends Component {
 	constructor() {
@@ -24,7 +28,22 @@ class SpeciesDetail extends Component {
 
 		return (
 			<View>
-				<Text>{species.name}</Text>
+				<Heading>{species.name}</Heading>
+				<Stats>
+					<Stat label="Height (avg)">{species.average_height}</Stat>
+					<Stat label="Lifespan (avg)">{species.average_lifespan}</Stat>
+					<Stat label="Classification">{species.classification}</Stat>
+					<Stat label="Designation">{species.designation}</Stat>
+					<Stat label="Eye Colors">{species.eye_colors}</Stat>
+					<Stat label="Hair Colors">{species.hair_colors}</Stat>
+					<Stat label="Language">{species.language}</Stat>
+					<Stat label="Skin Colors">{species.skin_colors}</Stat>
+					<Stat label="Homeworld"><LazyLink url={species.homeworld} association="planets"/></Stat>
+				</Stats>
+				<SectionHeading>Films ({species.films.length})</SectionHeading>
+				<LazyLinkList source={species} association="films"/>
+				<SectionHeading>People ({species.people.length})</SectionHeading>
+				<LazyLinkList source={species} association="people"/>
 			</View>
 		);
 	}
