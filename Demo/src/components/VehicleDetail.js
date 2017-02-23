27405e5e10ea948d94171e835799cee051e993a9
Bradley Spaulding <brad.spaulding@gmail.com>
2017-02-22 17:26:38 -0800

    Updated VehicleDetail

--- Demo/src/components/VehicleDetail.js
+++ Demo/src/components/VehicleDetail.js
@@ -3,6 +3,10 @@ import { View } from 'react-native';
 import { getVehicle } from '../api/SWApi';
 import Loading from './Loading';
 import Text from './Text';
+import Heading from './Heading';
+import SectionHeading from './SectionHeading';
+import LazyLinkList, { LazyLink } from './LazyLinkList';
+import { Stats, Stat } from './Stat';
 
 class SpeciesDetail extends Component {
 	constructor() {
@@ -24,7 +28,23 @@ class SpeciesDetail extends Component {
 
 		return (
 			<View>
-				<Text>{vehicle.name}</Text>
+				<Heading>{vehicle.name}</Heading>
+				<Stats>
+					<Stat label="Cargo Capacity">{vehicle.cargo_capacity}</Stat>
+					<Stat label="Consumables">{vehicle.consumables}</Stat>
+					<Stat label="Cost (credits)">{vehicle.cost_in_credits}</Stat>
+					<Stat label="Crew">{vehicle.crew}</Stat>
+					<Stat label="Length">{vehicle.length}</Stat>
+					<Stat label="Manufacturer">{vehicle.manufacturer}</Stat>
+					<Stat label="Speed (Max Atmos.)">{vehicle.max_atmosphering_speed}</Stat>
+					<Stat label="Model">{vehicle.model}</Stat>
+					<Stat label="Passengers">{vehicle.passengers}</Stat>
+					<Stat label="Class">{vehicle.vehicle_class}</Stat>
+				</Stats>
+				<SectionHeading>Films ({vehicle.films.length})</SectionHeading>
+				<LazyLinkList source={vehicle} association="films"/>
+				<SectionHeading>Pilots ({vehicle.pilots.length})</SectionHeading>
+				<LazyLinkList source={vehicle} accessor="pilots" association="people"/>
 			</View>
 		);
 	}
