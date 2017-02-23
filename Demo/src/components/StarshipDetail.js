7cd8744a5b57812422bda283e380d83d30207834
Bradley Spaulding <brad.spaulding@gmail.com>
2017-02-22 17:30:42 -0800

    Updated StarshipDetail

--- Demo/src/components/StarshipDetail.js
+++ Demo/src/components/StarshipDetail.js
@@ -3,6 +3,10 @@ import { View } from 'react-native';
 import { getStarship } from '../api/SWApi';
 import Loading from './Loading';
 import Text from './Text';
+import Heading from './Heading';
+import SectionHeading from './SectionHeading';
+import LazyLinkList, { LazyLink } from './LazyLinkList';
+import { Stats, Stat } from './Stat';
 
 class SpeciesDetail extends Component {
 	constructor() {
@@ -24,7 +28,25 @@ class SpeciesDetail extends Component {
 
 		return (
 			<View>
-				<Text>{starship.name}</Text>
+				<Heading>{starship.name}</Heading>
+				<Stats>
+					<Stat label="MGLT">{starship.MGLT}</Stat>
+					<Stat label="Cargo Capacity">{starship.cargo_capacity}</Stat>
+					<Stat label="Consumables">{starship.consumables}</Stat>
+					<Stat label="Cost (credits)">{starship.cost_in_credits}</Stat>
+					<Stat label="Crew">{starship.crew}</Stat>
+					<Stat label="Hyperdrive Rating">{starship.hyperdrive_rating}</Stat>
+					<Stat label="Length">{starship.length}</Stat>
+					<Stat label="Manufacturer">{starship.manufacturer}</Stat>
+					<Stat label="Speed (Max Atmos.)">{starship.max_atmosphering_speed}</Stat>
+					<Stat label="Model">{starship.model}</Stat>
+					<Stat label="Passengers">{starship.passengers}</Stat>
+					<Stat label="Class">{starship.starship_class}</Stat>
+				</Stats>
+				<SectionHeading>Films ({starship.films.length})</SectionHeading>
+				<LazyLinkList source={starship} association="films"/>
+				<SectionHeading>Pilots ({starship.pilots.length})</SectionHeading>
+				<LazyLinkList source={starship} accessor="pilots" association="people"/>
 			</View>
 		);
 	}
