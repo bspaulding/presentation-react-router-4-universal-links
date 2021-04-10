218cbb49229ef706d04ee863e23861f9e31c0ec9
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-04-10 13:32:58 -0700

    Fixed ListView getId regex

--- Demo/src/components/ListView.js
+++ Demo/src/components/ListView.js
@@ -1,60 +1,56 @@
-import React, { Component } from 'react';
-import { ListView as RNListView } from 'react-native';
-import { getFilms } from '../api/SWApi';
-import ListItem from './ListItem';
-import Loading from './Loading';
+import React, { Component } from "react";
+import { ListView as RNListView } from "react-native";
+import { getFilms } from "../api/SWApi";
+import ListItem from "./ListItem";
+import Loading from "./Loading";
 
 export const getId = name => {
-	const regex = new RegExp(`http:\/\/swapi.co\/api\/${name}\/(.*)\/`);
-	return url => {
-		const [_, id] = regex.exec(url);
-		return id;
-	};
+  const regex = new RegExp(`http:\/\/swapi.dev\/api\/${name}\/(.*)\/`);
+  return url => {
+    const [_, id] = regex.exec(url);
+    return id;
+  };
 };
 
 class ListView extends Component {
-	constructor(props) {
-		super();
+  constructor(props) {
+    super();
 
-		this.getId = getId(props.name);
-		this.state = {
-			dataSource: new RNListView.DataSource({
-				rowHasChanged: (a, b) => a !== b
-			}).cloneWithRows([]),
-			loading: true
-		};
-	}
+    this.getId = getId(props.name);
+    this.state = {
+      dataSource: new RNListView.DataSource({
+        rowHasChanged: (a, b) => a !== b
+      }).cloneWithRows([]),
+      loading: true
+    };
+  }
 
-	async componentDidMount() {
-		const response = await this.props.loader();
-		this.setState({
-			dataSource: this.state.dataSource.cloneWithRows(
-				response.results
-			),
-			loading: false
-		});
-	}
+  async componentDidMount() {
+    const response = await this.props.loader();
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(response.results),
+      loading: false
+    });
+  }
 
-	renderRow = (entity) => (
-		<ListItem to={`/${this.props.name}/${this.getId(entity.url)}`}>
-			{entity.name || entity.title}
-		</ListItem>
-	);
+  renderRow = entity => (
+    <ListItem to={`/${this.props.name}/${this.getId(entity.url)}`}>
+      {entity.name || entity.title}
+    </ListItem>
+  );
 
-	render() {
-		const { loading } = this.state;
-		return loading ? <Loading/> : (
-			<RNListView
-				dataSource={this.state.dataSource}
-				enableEmptySections={true}
-				renderRow={this.renderRow}
-			/>
-		);
-	}
+  render() {
+    const { loading } = this.state;
+    return loading ? (
+      <Loading />
+    ) : (
+      <RNListView
+        dataSource={this.state.dataSource}
+        enableEmptySections={true}
+        renderRow={this.renderRow}
+      />
+    );
+  }
 }
 
 export default ListView;
-
-
-
-
